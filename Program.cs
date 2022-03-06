4139baebff5312b828c96c5660ae3e042dc821bc
farhanfajarr <farhanfajarr@gmail.com>
2022-03-06 19:54:25 +0100

    new dotnet project with simple xml validator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Validator;
+
+namespace MainProgram
+{
+    public class Program
+    {
+        static void Main (String[] args) 
+        {
+
+            String xmlFilepath = "C:/Users/farha/Documents/git/simple-xml-validator/refxml/test1/breakfast-menu.xml";
+            String xsdFilePath = "C:/Users/farha/Documents/git/simple-xml-validator/refxml/test1/breakfast-menu.xsd";
+
+            XMLValidator xmlValidator = new XMLValidator(xmlFilepath, xsdFilePath);
+            xmlValidator.Validate();
+            
+        }
+    }
+}
